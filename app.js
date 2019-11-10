dff0a49be80b5492aa4c17bf0cdcea223133b31d
vlad <vlad19943103@gmail.com>
2019-11-10 13:38:17 +0100

    Finsh project

--- app.js
+++ app.js
@@ -4,13 +4,21 @@ const port = 3000;
 const app = express();
 const currentDay = require("./date.js")
 const day = currentDay();
+const listItems = ["Gym", "Running", "Washing", "Read", "Meet Vlad on airport"];
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("list", {currentDate: day});
+    res.render("list", {currentDate: day, items: listItems});
+});
+
+app.post("/", (req, res) => {
+    let itm = "";
+    itm = req.body.newItem;
+    listItems.push(itm);
+    res.redirect("/");
 });
 
 app.listen(port, () => {
